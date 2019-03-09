e3b82d5d049e13d4cc31ee38a9378861c1294ea3
Kate <kit.ty.kate@disroot.org>
2019-03-09 16:30:09 +0100

    Use type indirections instead of subtyping until the OCaml type-system has been fixed

--- lib/activitystreams.mli
+++ lib/activitystreams.mli
@@ -1,86 +1,88 @@
 type ('a, 'b) either = Left of 'a | Right of 'b
+type ('a, 'b, 'c, 'd) either4 = Case1 of 'a | Case2 of 'b | Case3 of 'c | Case4 of 'd
 type anyURI = Uri.t
+type dateTime = unit (* TODO *)
+type duration = unit (* TODO *)
+type nonNegativeInteger = unit (* TODO *)
+type units = Cm | Feet | Inches | Km | Meter | Miles
 
-type t = [
-  | ty_objects
-  | ty_link
-]
+(* TODO: Replace type indirections with polymorphic variants once the OCaml type-system has been fixed *)
 
-and ty_objects = [
+type t =
+  | Ap_objects of ty_object
+  | Ap_links of ty_link
+
+and ty_object =
   (* Core Types *)
-  | `Object of ap_object
-  | `Activity of ap_activity
-  | `IntransitiveActivity of ap_intransitiveActivity
-  | ty_collection
+  | Object of ap_object
+  | Activity of ap_activity
+  | IntransitiveActivity of ap_intransitiveActivity
+  | Ap_collections of ty_collection
   (* Activity Types *)
-  | `Accept of ap_activity
-  | `TentativeAccept of ap_activity
-  | `Add of ap_activity
-  | `Arrive of ap_intransitiveActivity
-  | `Create of ap_activity
-  | `Delete of ap_activity
-  | `Follow of ap_activity
-  | `Ignore of ap_activity
-  | `Join of ap_activity
-  | `Leave of ap_activity
-  | `Like of ap_activity
-  | `Offer of ap_activity
-  | `Invite of ap_activity
-  | `Reject of ap_activity
-  | `TentativeReject of ap_activity
-  | `Remove of ap_activity
-  | `Undo of ap_activity
-  | `Update of ap_activity
-  | `View of ap_activity
-  | `Listen of ap_activity
-  | `Read of ap_activity
-  | `Move of ap_activity
-  | `Travel of ap_intransitiveActivity
-  | `Announce of ap_activity
-  | `Block of ap_activity
-  | `Flag of ap_activity
-  | `Dislike of ap_activity
-  | `Question of ap_question
+  | Accept of ap_activity
+  | TentativeAccept of ap_activity
+  | Add of ap_activity
+  | Arrive of ap_intransitiveActivity
+  | Create of ap_activity
+  | Delete of ap_activity
+  | Follow of ap_activity
+  | Ignore of ap_activity
+  | Join of ap_activity
+  | Leave of ap_activity
+  | Like of ap_activity
+  | Offer of ap_activity
+  | Invite of ap_activity
+  | Reject of ap_activity
+  | TentativeReject of ap_activity
+  | Remove of ap_activity
+  | Undo of ap_activity
+  | Update of ap_activity
+  | View of ap_activity
+  | Listen of ap_activity
+  | Read of ap_activity
+  | Move of ap_activity
+  | Travel of ap_intransitiveActivity
+  | Announce of ap_activity
+  | Block of ap_activity
+  | Flag of ap_activity
+  | Dislike of ap_activity
+  | Question of ap_question
   (* Actor Types *)
-  | `Application of ap_object
-  | `Group of ap_object
-  | `Organization of ap_object
-  | `Person of ap_object
-  | `Service of ap_object
+  | Application of ap_object
+  | Group of ap_object
+  | Organization of ap_object
+  | Person of ap_object
+  | Service of ap_object
   (* Object Types *)
-  | `Relationship of ap_relationship
-  | `Article of ap_object
-  | `Document of ap_object
-  | `Audio of ap_object
-  | `Image of ap_object
-  | `Video of ap_object
-  | `Note of ap_object
-  | `Page of ap_object
-  | `Event of ap_object
-  | `Place of ap_place
-  | `Profile of ap_profile
-  | `Tombstone of ap_tombstone
-]
+  | Relationship of ap_relationship
+  | Article of ap_object
+  | Document of ap_object
+  | Audio of ap_object
+  | Image of ap_object
+  | Video of ap_object
+  | Note of ap_object
+  | Page of ap_object
+  | Event of ap_object
+  | Place of ap_place
+  | Profile of ap_profile
+  | Tombstone of ap_tombstone
 
-and ty_link = [
+and ty_link =
   (* Core Types *)
-  | `Link of ap_link
+  | Link of ap_link
   (* Link Types *)
-  | `Mention of ap_link
-]
+  | Mention of ap_link
 
-and ty_collection = [
-  | `Collection of ap_collection
-  | `OrderedCollection of ap_collection
-  | ty_collectionPage
-]
+and ty_collection =
+  | Collection of ap_collection
+  | OrderedCollection of ap_collection
+  | Ap_collectionPage of ty_collectionPage
 
-and ty_collectionPage = [
-  | `CollectionPage of ap_collectionPage
-  | `OrderedCollectionPage of ap_orderedCollectionPage
-]
+and ty_collectionPage =
+  | CollectionPage of ap_collectionPage
+  | OrderedCollectionPage of ap_orderedCollectionPage
 
-and ap_object = <
+and ap_object = {
   id : prop_id; (* Not defined ??! *)
   type_ : prop_type; (* Not defined ??! *)
   attachment : prop_attachment;
@@ -109,9 +111,9 @@ and ap_object = <
   bcc : prop_bcc;
   mediaType : prop_mediaType;
   duration : prop_duration;
->
+}
 
-and ap_link = <
+and ap_link = {
   id : prop_id; (* Not defined ??! *)
   type_ : prop_type; (* Not defined ??! *)
   href : prop_href;
@@ -122,83 +124,83 @@ and ap_link = <
   height : prop_height;
   width : prop_width;
   preview : prop_preview;
->
+}
 
-and ap_activity = <
-  ap_object;
+and ap_activity = {
+  ap_object : ap_object;
   actor : prop_actor;
   object_ : prop_object;
   target : prop_target;
   result : prop_result;
   origin : prop_origin;
   instrument : prop_instrument;
->
+}
 
-and ap_intransitiveActivity = <
-  ap_object;
+and ap_intransitiveActivity = {
+  ap_object : ap_object;
   actor : prop_actor;
   target : prop_target;
   result : prop_result;
   origin : prop_origin;
   instrument : prop_instrument;
->
+}
 
-and ap_collection = <
-  ap_object;
+and ap_collection = {
+  ap_object : ap_object;
   totalItems : prop_totalItems;
   current : prop_current;
   first : prop_first;
   last : prop_last;
   items : prop_items;
->
+}
 
-and ap_collectionPage = <
-  ap_collection;
+and ap_collectionPage = {
+  ap_collection : ap_collection;
   partOf : prop_partOf;
   next : prop_next;
   prev : prop_prev;
->
+}
 
-and ap_orderedCollectionPage = <
-  ap_orderedCollection;
-  ap_collectionPage;
+and ap_orderedCollectionPage = {
+  ap_collection : ap_collection;
+  ap_collectionPage : ap_collectionPage;
   startIndex : prop_startIndex;
->
+}
 
-and ap_question = <
-  ap_intransitiveActivity;
+and ap_question = {
+  ap_intransitiveActivity : ap_intransitiveActivity;
   oneOf : prop_oneOf;
   anyOf : prop_anyOf;
   closed : prop_closed;
->
+}
 
-and ap_relationship = <
-  ap_object;
+and ap_relationship = {
+  ap_object : ap_object;
   subject : prop_subject;
   object_ : prop_object;
   relationship : prop_relationship;
->
+}
 
-and ap_place = <
-  ap_object;
+and ap_place = {
+  ap_object : ap_object;
   accuracy : prop_accuracy;
   altitude : prop_altitude;
   latitude : prop_latitude;
   longitude : prop_longitude;
   radius : prop_radius;
   units : prop_units;
->
+}
 
-and ap_profile = <
-  ap_object;
+and ap_profile = {
+  ap_object : ap_object;
   describes : prop_describes;
->
+}
 
-and ap_tombstone = <
-  ap_object;
+and ap_tombstone = {
+  ap_object : ap_object;
   formerType : prop_formerType;
   deleted : prop_deleted;
->
+}
 
 and prop_id = anyURI option
 and prop_type = anyURI list
@@ -212,9 +214,9 @@ and prop_cc = (ty_object, ty_link) either list list
 and prop_context = (ty_object, ty_link) either list list
 and prop_current = (ty_collectionPage, ty_link) either option
 and prop_first = (ty_collectionPage, ty_link) either option
-and prop_generator = (ty_object, ty_lin) either list list
-and prop_icon = (ty_image, ty_link) either list list
-and prop_image = (ty_image, ty_link) either list list
+and prop_generator = (ty_object, ty_link) either list list
+and prop_icon = (ap_object, ty_link) either list list
+and prop_image = (ap_object, ty_link) either list list
 and prop_inReplyTo = (ty_object, ty_link) either list list
 and prop_instrument = (ty_object, ty_link) either list list
 and prop_last = (ty_collectionPage, ty_link) either option
